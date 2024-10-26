1d8dfb9699c052cebd7aa3c48aa00904cf3f0115
phatnguyen <phatnguyen@gmx.de>
2024-10-26 11:39:05 +0200

    cp

--- db/index.ts
+++ db/index.ts
@@ -2,8 +2,8 @@ import Database from 'better-sqlite3'
 import { drizzle } from 'drizzle-orm/better-sqlite3'
 import * as schema from './schema'
 
-const sqlite = new Database('./db/db.sqlite3')
+const sqlite = new Database('./db/db.sqlite3', { fileMustExist: true })
 export const db = drizzle({ client: sqlite, schema })
-console.log(db.run('SELECT * FROM embeddings;'))
+
 export * from './schema'
 export * from './types'
--- drizzle/schema.ts
+++
@@ -1,8 +0,0 @@
-import { sql } from 'drizzle-orm'
-import { AnySQLiteColumn, integer, sqliteTable, text } from 'drizzle-orm/sqlite-core'
-
-export const embeddings = sqliteTable('embeddings', {
-  id: integer().primaryKey({ autoIncrement: true }).notNull(),
-  input: text().notNull(),
-  value: text().notNull(),
-})
