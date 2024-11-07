70cc92decf707bd1ffc5670a6b4b179f73dae574
phatnguyen <phatnguyen@gmx.de>
2024-11-08 00:02:41 +0100

    feat: add documents and sets tables to database schema; initialize journal and snapshot metadata

--- db/schema.ts
+++ db/schema.ts
@@ -1,14 +1,9 @@
-import { sql } from 'drizzle-orm'
-import { blob, check, integer, sqliteTable, text } from 'drizzle-orm/sqlite-core'
+import { blob, integer, sqliteTable, text } from 'drizzle-orm/sqlite-core'
 
 export const documents = sqliteTable('documents', {
   id: integer().primaryKey({ autoIncrement: true }).notNull(),
   contents: text().notNull(),
   embedding: blob(),
-}, () => {
-  return {
-    documentsCheck1: check('documents_check_1', sql`vec_length(embedding)==768`),
-  }
 })
 
 export const sets = sqliteTable('sets', {
--- server/api/docs/embed.post.ts
+++ server/api/docs/embed.post.ts
@@ -3,19 +3,19 @@ import { db, sqlite } from '~/db'
 export default defineEventHandler(async (event) => {
   const body = await readBody(event)
 
-  //   const script = `
+  // const script = `
   //   INSERT INTO documents (contents, embedding) VALUES (?, vec_f32(?));
   // `
-  //   const stmt = sqlite.prepare(script)
-  //   stmt.run(body.contents, JSON.stringify(body.embedding))
+  // const stmt = sqlite.prepare(script)
+  // stmt.run(body.contents, JSON.stringify(body.embedding))
 
-  const script = `
-    select
-      id,
-      vec_distance_cosine(embedding, ?) as distance
-    from documents
-    order by distance;
-  `
-  const a = sqlite.prepare(script).all(JSON.stringify(body.embedding))
-  console.log(a)
+  // const script = `
+  //   select
+  //     id,
+  //     vec_distance_cosine(embedding, ?) as distance
+  //   from documents
+  //   order by distance;
+  // `
+  // const a = sqlite.prepare(script).all(JSON.stringify(body.embedding))
+  // console.log(a)
 })
