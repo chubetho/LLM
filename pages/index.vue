141f851d65c6e2482c6676da8b66bd77db94ebf7
phatnguyen <phatnguyen@gmx.de>
2024-11-08 17:30:40 +0100

    feat: update UI layout and styles; enhance search input, adjust card styles, and modify database schema for sets

--- pages/index.vue
+++ pages/index.vue
@@ -11,43 +11,49 @@ const { data: sets } = await useFetch('/api/sets')
       all quizzes
     </template>
 
-    <ul class="grid gap-3 grid-cols-4">
-      <li>
-        <Card
-          class="group hover:border-primary transition-colors cursor-pointer h-full flex items-center justify-center min-h-40"
-          @click="navigateTo('/insert')"
-        >
-          <CirclePlus class="size-16 stroke-[0.5px] stroke-primary/60 group-hover:stroke-primary group-hover:stroke-[1px] transition-colors" />
-        </Card>
-      </li>
-      <template v-if="sets">
-        <li v-for="set in sets" :key="set.id">
-          <NuxtLink class="group" :to="`/sets/${set.id}`">
-            <Card class="group-hover:border-primary transition-colors h-full flex flex-col">
-              <CardHeader>
-                <CardTitle>{{ set.name }}</CardTitle>
-              </CardHeader>
-              <CardContent>
-                <ul class="flex flex-wrap gap-1 pb-3">
-                  <li v-for="tag in set.tags" :key="tag">
-                    <Badge variant="secondary">
-                      {{ tag }}
-                    </Badge>
-                  </li>
-                </ul>
-              </CardContent>
-              <CardFooter class="text-xs text-muted-foreground flex justify-between mt-auto">
-                <span>
-                  Created at {{ set.createAt }}
-                </span>
-                <span>
-                  ({{ set.cards.length }})
-                </span>
-              </CardFooter>
-            </Card>
-          </NuxtLink>
+    <div class="space-y-4 p-1">
+      <div class="">
+        <Input placeholder="search anything..." />
+      </div>
+
+      <ul class="grid gap-3 grid-cols-4">
+        <li>
+          <Card
+            class="group hover:border-primary transition-colors cursor-pointer h-full flex items-center justify-center min-h-40"
+            @click="navigateTo('/insert')"
+          >
+            <CirclePlus class="size-16 stroke-[0.5px] stroke-primary/60 group-hover:stroke-primary group-hover:stroke-[1px] transition-colors" />
+          </Card>
         </li>
-      </template>
-    </ul>
+        <template v-if="sets">
+          <li v-for="set in sets" :key="set.id">
+            <NuxtLink class="group" :to="`/sets/${set.id}`">
+              <Card class="group-hover:border-primary transition-colors h-full flex flex-col">
+                <CardHeader>
+                  <CardTitle>{{ set.name }}</CardTitle>
+                </CardHeader>
+                <CardContent>
+                  <ul class="flex flex-wrap gap-1 pb-3">
+                    <li v-for="tag in set.tags" :key="tag">
+                      <Badge variant="secondary">
+                        {{ tag }}
+                      </Badge>
+                    </li>
+                  </ul>
+                </CardContent>
+                <CardFooter class="text-xs text-muted-foreground flex justify-between mt-auto">
+                  <span>
+                    Created at {{ set.createAt }}
+                  </span>
+                  <span>
+                    ({{ set.cards.length }})
+                  </span>
+                </CardFooter>
+              </Card>
+            </NuxtLink>
+          </li>
+        </template>
+      </ul>
+    </div>
   </BasePageWrap>
 </template>
